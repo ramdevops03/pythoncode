0790aac88d3997ee231667b2224687a65d0d886a
ramdevops03 <79424528+ramdevops03@users.noreply.github.com>
2022-05-24 19:35:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hi, Welcome to MagTech"
+    return "Hi, Welcome to Computer Science Guru...."
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
-
-# Why you should run it at 0.0.0.0
-# https://stackoverflow.com/questions/30323224/deploying-a-minimal-flask-app-in-docker-server-connection-issues
